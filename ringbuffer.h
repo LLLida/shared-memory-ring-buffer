952dd67a42ed27668443f9691e1c1b5d18f46cef
LLLida <0adilmohammad0@gmail.com>
2024-06-30 20:46:55 +0300

    Write about SeqLock in README

--- ringbuffer.h
+++ ringbuffer.h
@@ -14,6 +14,12 @@
 #include "time.h"
 #include "unistd.h"
 
+/* if using mutex than latency gets higher 20-100 times but energy
+   consumption of CPU drastically decreases */
+#ifndef RING_BUFFER_USE_MUTEX
+#define RING_BUFFER_USE_MUTEX 0
+#endif
+
 /* per-process object */
 struct Ring_Buffer {
   /* pointer to begining of ring buffer (shared memory)
@@ -35,11 +41,14 @@ struct Ring_Buffer_Details {
   size_t tail;
   int fd;
   char identifier[64];
+#if RING_BUFFER_USE_MUTEX
   pthread_mutexattr_t mutex_attr;
   pthread_mutex_t mutex;
   pthread_condattr_t cond_attr;
   pthread_cond_t cond;
+#else
   _Atomic size_t version;
+#endif
   _Atomic size_t refcount;
 };
 
@@ -48,8 +57,10 @@ void detach_ring_buffer(struct Ring_Buffer_Details* rb)
   size_t val = --rb->refcount;
 
   if (val == 0) {
+#if RING_BUFFER_USE_MUTEX
     pthread_cond_destroy(&rb->cond);
     pthread_mutex_destroy(&rb->mutex);
+#endif
     char buff[64];
     strcpy(buff, rb->identifier);
 
@@ -74,7 +85,7 @@ int create_ring_buffer(struct Ring_Buffer* ring_buffer, const char* identifier, 
 
      Using virtual memory magic we make so that there are 2 blocks of
      pages that point to same physical memory.
-   */
+  */
 
   const int pagesize = getpagesize();
   if (size % pagesize != 0) {
@@ -110,10 +121,10 @@ int create_ring_buffer(struct Ring_Buffer* ring_buffer, const char* identifier, 
   rb->size = size;
   rb->head = 0;
   rb->tail = 0;
-  rb->version = 0;
   rb->refcount = 1;
   strncpy(rb->identifier, identifier, sizeof(rb->identifier)-1);
   rb->fd = fd;
+#if RING_BUFFER_USE_MUTEX
   pthread_mutexattr_init(&rb->mutex_attr);
   pthread_mutexattr_setpshared(&rb->mutex_attr, PTHREAD_PROCESS_SHARED);
   if (pthread_mutex_init(&rb->mutex, &rb->mutex_attr) != 0) {
@@ -128,6 +139,9 @@ int create_ring_buffer(struct Ring_Buffer* ring_buffer, const char* identifier, 
     detach_ring_buffer(rb);
     return -1;
   }
+#else
+  rb->version = 0;
+#endif
 
   *ring_buffer = (struct Ring_Buffer) {
     .buffer = buffer,
@@ -184,19 +198,25 @@ int write_to_ring_buffer(struct Ring_Buffer* ring_buffer, const void* data, size
     return -1;
   }
 
-  /* pthread_mutex_lock(&rb->mutex); */
-  /* if (rb->size - (rb->tail-rb->head) < size) { */
-  /*   pthread_mutex_unlock(&rb->mutex); */
-  /*   return 1; */
-  /* } */
+#if RING_BUFFER_USE_MUTEX
+  pthread_mutex_lock(&rb->mutex);
+  if (rb->size - (rb->tail-rb->head) < size) {
+    pthread_mutex_unlock(&rb->mutex);
+    return 1;
+  }
+#else
   rb->version += 1;
+#endif
 
   memcpy(&buffer[rb->tail], data, size);
   rb->tail += size;
 
+#if RING_BUFFER_USE_MUTEX
+  pthread_cond_signal(&rb->cond);
+  pthread_mutex_unlock(&rb->mutex);
+#else
   rb->version += 1;
-  /* pthread_cond_signal(&rb->cond); */
-  /* pthread_mutex_unlock(&rb->mutex); */
+#endif
   return 0;
 }
 
@@ -207,14 +227,17 @@ void* read_from_ring_buffer(struct Ring_Buffer* ring_buffer, size_t* size)
   struct Ring_Buffer_Details* rb = ring_buffer->impl;
 
   void* data = NULL;
-  /* pthread_mutex_lock(&rb->mutex); */
-  /* while(rb->head == rb->tail) { */
-  /*   pthread_cond_wait(&rb->cond, &rb->mutex); */
-  /* } */
+#if RING_BUFFER_USE_MUTEX
+  pthread_mutex_lock(&rb->mutex);
+  while(rb->head == rb->tail) {
+    pthread_cond_wait(&rb->cond, &rb->mutex);
+  }
+#else
   size_t value = rb->version;
   while (value & 1) {
     value = rb->version;
   }
+#endif
 
   *size = rb->tail - rb->head;
   data = &buffer[rb->head];
@@ -225,7 +248,9 @@ void* read_from_ring_buffer(struct Ring_Buffer* ring_buffer, size_t* size)
     rb->tail -= rb->size;
   }
 
-  /* pthread_mutex_unlock(&rb->mutex); */
+#if RING_BUFFER_USE_MUTEX
+  pthread_mutex_unlock(&rb->mutex);
+#endif
   return data;
 }
 
